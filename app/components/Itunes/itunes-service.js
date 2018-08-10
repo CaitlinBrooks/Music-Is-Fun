c6df1743743bcee988415465fd7680a170f10464
CaitlinCodes <caiit@me.com>
2018-08-10 15:13:15 -0600

    index and itunes

--- app/components/Itunes/itunes-service.js
+++ app/components/Itunes/itunes-service.js
@@ -3,19 +3,42 @@ import { Song } from "../../models/Song.js";
 
 class ItunesService {
 
-  //DO NOT MODIFY
   getMusicByArtist(artist) {
-    var url = 'https://itunes.apple.com/search?callback=?&term=' + artist;
+
+    var url1 = '//bcw-getter.herokuapp.com/?url=';
+    var url2 = 'https://itunes.apple.com/search?term=' + artist
+    var apiUrl = url1 + encodeURIComponent(url2);
     //Casts each object to 
     // @ts-ignore
-    return $.getJSON(url).then(function (response) {
-      var songList = response.results.map(s => { return new Song(s) })
+    return $.getJSON(apiUrl).then(function (response) {
+      var songList = response.results.map(s => {
+        return new Song(s)
+      })
       return songList;
     })
   }
 
 
 }
+
+// USE WHEN I GET HOME
+// class ItunesService {
+
+//   //DO NOT MODIFY
+//   getMusicByArtist(artist) {
+//     var url = 'https://itunes.apple.com/search?callback=?&term=' + artist;
+//     //Casts each object to 
+//     // @ts-ignore
+//     return $.getJSON(url).then(function (response) {
+//       var songList = response.results.map(s => { return new Song(s) })
+//       return songList;
+//     })
+//   }
+
+
+// }
+
+
 
 
 
