512c496284cfa3c57368caa2b8ef0e615bf7355f
CaitlinCodes <caiit@me.com>
2018-08-10 10:04:13 -0600

    framework setup

---
+++ app/components/Itunes/itunes-controller.js
@@ -0,0 +1,37 @@
+import ItunesService from "./itunes-service.js";
+
+//PRIVATE
+
+const itunesService = new ItunesService()
+
+function draw(results) {
+  console.log(results)
+  //YOUR CODING STARTS HERE
+
+
+
+}
+
+
+//PUBLIC
+class ItunesController {
+  //DO NOT MODIFY THIS METHOD
+  getMusic(e) {
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    //changes the button to loading while songs load
+    // @ts-ignore
+    $('#get-music-button').text('LOADING....');
+    itunesService.getMusicByArtist(artist).then(results => {
+      draw(results)
+      //changes button back to GET MUSIC once songs are loaded
+      // @ts-ignore
+      $('#get-music-button').text('GET MUSIC');
+    })
+  }
+
+
+}
+
+
+export default ItunesController
