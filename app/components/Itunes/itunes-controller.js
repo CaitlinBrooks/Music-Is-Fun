56fd5f94a0aad3cfa322f30ea0f84985e5896f63
CaitlinCodes <caiit@me.com>
2018-08-10 16:13:56 -0600

    buttons css and index

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -13,9 +13,9 @@ function draw(songs) {
     const song = songs[i];
     template += `
     <li class="media">
-    <img class="mr-3" src="${song.albumArt}" alt="placehold.it">
+    <img class="mr-3 mt-2 ml-3" src="${song.albumArt}" alt="placehold.it">
     <div class="media-body">
-        <h5 class="mt-0 mb-1">${song.title}</h5>
+        <h5 class="mt-0 mt-2 mb-1">${song.title}</h5>
         <p> ${song.artist}</p>
         <audio controls>
             <source src="${song.preview}" type="audio/mpeg">
