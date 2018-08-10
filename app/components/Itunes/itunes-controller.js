e87482422baaf2e008575a486ccbb9c63c47aa3c
CaitlinCodes <caiit@me.com>
2018-08-10 11:41:55 -0600

    draw songs function to index

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -1,15 +1,28 @@
 import ItunesService from "./itunes-service.js";
+import { Song } from "../../models/Song.js";
 
 //PRIVATE
 
 const itunesService = new ItunesService()
 
-function draw(results) {
-  console.log(results)
+function draw(songs) {
   //YOUR CODING STARTS HERE
-
-
-
+  //draw the items to the page
+  let template = ''
+  for (let i = 0; i < songs.length; i++) {
+    const song = songs[i];
+    template += `
+    <div class="form-inline" onclick="app.controllers.itunesCtrl.getMusic(event)(${i})">
+     <img src="${song.albumArt}" alt = "song.previewUrl">
+     <p> ${song.title}</p> 
+     <p> ${song.artist}</p>
+     <p> ${song.price}</p>
+    </div>
+    `
+    //I want to save a thumbnail, an artist, a preview, a collection (CD), and a price to update to the index and inject new data.
+  }
+  document.getElementById('song-list').innerHTML = template
+  console.log(songs)
 }
 
 
